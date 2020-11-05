86e0ae05ca2835e423c42c719577480fe6bfca8f
daveedoo <damaks48@gmail.com>
2020-11-05 21:25:36 +0100

    Project created and triangles drawing

---
+++ Filling/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Filling
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        private int N = 10;
+        private int M = 10;
+
+        private Triangle[] Triangles;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+
+            int w = pictureBox.Width / N;
+            int h = pictureBox.Height / M;
+            Triangles = new Triangle[2 * N * M];
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    Triangles[2 * i * N + 2 * j] = new Triangle(new Point(w * j, h * i), new Point(w * (j + 1), h * i), new Point(w * j, h * (i + 1)));
+                    Triangles[2 * i * N + 2 * j + 1] = new Triangle(new Point(w * (j + 1), h * (i + 1)), new Point(w * (j + 1), h * i), new Point(w * j, h * (i + 1)));
+                }
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap newBitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            for (int i = 0; i < 2*N*M; i++)
+            {
+                Triangles[i].Draw(newBitmap);
+            }
+            pictureBox.Image.Dispose();
+            pictureBox.Image = newBitmap;
+        }
+    }
+}
---
+++ Filling/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filling
+{
+    public class Triangle
+    {
+        private Point P1, P2, P3;
+
+        public Triangle(Point p1, Point p2, Point p3)
+        {
+            P1 = p1;
+            P2 = p2;
+            P3 = p3;
+        }
+
+        public void Draw(Bitmap bmp)
+        {
+            Graphics G = Graphics.FromImage(bmp);
+            G.DrawLine(Pens.Black, P1, P2);
+            G.DrawLine(Pens.Black, P2, P3);
+            G.DrawLine(Pens.Black, P3, P1);
+            G.Dispose();
+        }
+    }
+}
